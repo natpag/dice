f682ff3945b86b353a7cf025cf974353ca404336
npagan <npagan@proforma.com>
2020-03-23 14:17:37 -0400

    diceBellwork

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
 
-const App = () => {
-  return <HelloWorld />
+class App extends Component {
+  state = {
+    diceResult: 0,
+  }
+
+  rollDice = (e) => {
+    const result = Math.round(Math.random() * 6)
+    console.log('Rolling dice', result)
+    this.setState({
+      diceResult: result,
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <div class="d6">
+          <h1>{this.state.diceResult}</h1>
+          <button onClick={this.rollDice}>Roll D6</button>
+        </div>
+      </>
+    )
+  }
 }
 
 export default App
